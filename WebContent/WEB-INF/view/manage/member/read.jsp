b199e733c3978353ecd7aa9d8a301a033edcef2f
Yun <tssu106@gmail.com>
2019-01-09 10:10:23 +0900

    관리자 회원 리스트 추가

---
+++ WebContent/WEB-INF/view/manage/member/read.jsp
@@ -0,0 +1,98 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<%@ page import="member.*" %>
+<%
+MemberVO param = (MemberVO)request.getAttribute("vo");
+MemberVO data = (MemberVO)request.getAttribute("data");
+%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko" lang="ko">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<%@ include file="/WEB-INF/view/manage/include/headHtml.jsp" %>
+<script type="text/javascript">
+	function goDelete() {
+		var del = confirm ('삭제하시겠습니까?');
+		if (del){
+			document.location.href = "process?mno=<%=data.getMno()%>&cmd=delete";
+		} else {
+			return false;
+		}
+	}
+</script>
+</head>
+<body> 
+<%@ include file="/WEB-INF/view/manage/include/common.jsp" %>
+<div id="wrap">
+	<!-- canvas -->
+	<div id="canvas">
+		<!-- S T A R T :: headerArea-->
+		<%@ include file="/WEB-INF/view/manage/include/top.jsp" %>
+		<!-- E N D :: headerArea--> 
+		
+		<!-- S T A R T :: containerArea-->
+		<div id="container">
+			<div id="content">
+				<div class="con_tit">
+					<h2>회원관리 - [상세]</h2>
+				</div>
+				<!-- //con_tit -->
+				<div class="con">
+					<!-- 내용 : s -->
+					<div id="bbs">
+						<div id="bread">
+							<h3>기본 정보</h3>
+							<table width="100%" border="0" cellspacing="0" cellpadding="0" summary="관리자 관리 기본내용입니다.">
+								<colgroup>
+									<col width="15%" />
+									<col width="35%" />
+									<col width="15%" />
+									<col width="35%" />
+								</colgroup>
+								<tbody>
+									<tr>
+										<th scope="row"><label for="">이름</label></th>
+										<td><%=data.getMname()%></td>
+									</tr>
+									<tr>
+										<th scope="row"><label for="">이메일</label></th>
+										<td><%=data.getEmail()%></td>
+									</tr>
+									<tr>
+										<th scope="row"><label for="">가입일</label></th>
+										<td><%=data.getCre_date()%></td>
+									</tr>
+									<tr>
+										<th scope="row"><label for="">암호변경일</label></th>
+										<td><%=data.getMod_date()%></td>
+									</tr>
+								</tbody>
+							</table>
+							<div class="btn">
+								<div class="btnLeft">
+									<a class="btns" href="<%=param.getTargetURLParam("index", param, 0)%>"><strong>목록</strong></a>
+								</div>
+								<div class="btnRight">
+									<a class="btns" href="<%=param.getTargetURLParam("edit", param, data.getMno())%>"><strong>수정</strong></a>
+									<a class="btns" href="#" onClick="goDelete();"><strong>삭제</strong></a>
+								</div>
+							</div>
+							<!--//btn-->
+						</div>
+						<!-- //bread -->
+					</div>
+					<!-- //bbs --> 
+					<!-- 내용 : e -->
+				</div>
+				<!--//con -->
+			</div>
+			<!--//content -->
+		</div>
+		<!--//container --> 
+		<!-- E N D :: containerArea-->
+	</div>
+	<!--//canvas -->
+</div>
+<!--//wrap -->
+
+</body>
+</html>
---
+++ src/member/MemberVO.java
@@ -0,0 +1,153 @@
+package member;
+
+import java.sql.Timestamp;
+
+import util.Parameter;
+
+public class MemberVO extends Parameter {
+
+	private int no;			  //회원번호
+	private String name;      //회원명
+	private String birth;     //생년월일
+	private String email;	  //이메일(id)
+	private String pw;        //비밀번호
+	private int gender;       //성별 "1=남자" "2=여자"
+	private String hp;        //휴대폰번호
+	private int point;        //영화 포인트
+	private Timestamp credate;//회원 가입일
+	private Timestamp moddate;//회원정보 수정일
+	
+	
+	
+	private int prev_no;			// 이전글 no
+	private int next_no;			// 다음글 no
+	private String prev_title;		// 이전글 제목
+	private String next_title;		// 다음글 제목
+
+	public MemberVO() {
+		super.setPageRows(10);
+	}
+	/**
+	 * 생성자 페이지로우수 세팅
+	 * @param pageRows 페이지 로우수
+	 */
+	public MemberVO(int pageRows) {
+		super.setPageRows(pageRows);
+	}
+	
+	/**
+	 *  검색조건 파라미터
+	 *  read에서 index등으로 검색조건 그대로 이동할때
+	 *  답글작성 후 다시 read페이지로 돌아갈때 no가 필요하므로 index값 설정
+	 *  그 외 필요한 값들은 각 VO에 세팅할 것.
+	 * @param pageUrl target URL
+	 * @param param Parameter
+	 * @param index no값필요시
+	 * @return String
+	 */
+	public String getTargetURLParam(String pageUrl, MemberVO param, int index) {
+
+		StringBuffer strList= new StringBuffer();
+
+		if (param != null) {
+			strList.append(pageUrl);
+			strList.append("?stype=" + param.getStype());
+			strList.append("&sval="+param.getSval());
+			strList.append("&reqPageNo="+param.getReqPageNo());
+			if (index > 0) {
+				strList.append("&mno="+index);
+			}
+		}
+		return strList.toString();
+	}
+	
+	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public Timestamp getCredate() {
+		return credate;
+	}
+	public void setCredate(Timestamp credate) {
+		this.credate = credate;
+	}
+	public Timestamp getModdate() {
+		return moddate;
+	}
+	public void setModdate(Timestamp moddate) {
+		this.moddate = moddate;
+	}
+	
+	
+	public int getPrev_no() {
+		return prev_no;
+	}
+	public int getNext_no() {
+		return next_no;
+	}
+	public String getPrev_title() {
+		return prev_title;
+	}
+	public String getNext_title() {
+		return next_title;
+	}
+	public void setPrev_no(int prev_no) {
+		this.prev_no = prev_no;
+	}
+	public void setNext_no(int next_no) {
+		this.next_no = next_no;
+	}
+	public void setPrev_title(String prev_title) {
+		this.prev_title = prev_title;
+	}
+	public void setNext_title(String next_title) {
+		this.next_title = next_title;
+	}
+
+}
